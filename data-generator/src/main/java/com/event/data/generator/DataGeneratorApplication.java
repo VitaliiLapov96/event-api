ae49f44e4553e40843397e211e3834e0648eb943
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2023-05-24 12:17:12 +0300

    microservices-and-security: refactor data-generator

--- data-generator/src/main/java/com/event/data/generator/DataGeneratorApplication.java
+++ data-generator/src/main/java/com/event/data/generator/DataGeneratorApplication.java
@@ -2,8 +2,10 @@ package com.event.data.generator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DataGeneratorApplication {
 
     public static void main(String[] args) {
