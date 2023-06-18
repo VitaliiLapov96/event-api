2eeb109d3eaf39de2a95bcd4825d97ee65d2fc1c
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2023-06-18 18:01:15 +0300

    microservices-and-security: add empty line

--- core/src/main/java/com/event/core/EventApplication.java
+++ core/src/main/java/com/event/core/EventApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class EventApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EventApplication.class, args);
 	}
 
